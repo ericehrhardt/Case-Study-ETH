42db60dd3f8948ad81faa90a10f46a3b65a17cef
dberti2000 <dberti@student.ethz.ch>
2023-04-26 16:44:43 +0200

    last version policy

--- H2CS2/src/02_setup/setup_policy.jl
+++ H2CS2/src/02_setup/setup_policy.jl
@@ -6,7 +6,7 @@ function setup_policy(model::Model, inputs::InputStruct)
     for i in 1:nprod
         for y in 1:nyear
             for h in 1:nhour
-                add_to_expression!(model[:obj], Emission_Factor(inputs, i,y)* Weight_Hour(inputs, y,h) * q[prod[i], years[y], hours[h]])
+                add_to_expression!(model[:obj], Emission_Factor(inputs, i,y)* Discount_Factor(inputs,y) * Weight_Hour(inputs, y,h) * q[prod[i], years[y], hours[h]])
             end
         end
     end
