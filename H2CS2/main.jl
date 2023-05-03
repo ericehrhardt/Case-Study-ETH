b603a6400073322128576c10ef91127524ddbcb0
Christoph Funke <csfunke@yahoo.com>
2023-05-04 00:41:09 +0200

    Upload new input file The new input file has the most up-to-date data. Storage is still not included.

--- H2CS2/main.jl
+++ H2CS2/main.jl
@@ -4,10 +4,10 @@ using JuMP
 
 #Case Parameters ---------------------------------------------------------------
 input_filepath = "./inputs/"
-input_filename = "input.xlsx"
+input_filename = "input_v2.xlsx"
 
 output_filepath = "./outputs/"
-output_filename = "output.xlsx"
+output_filename = "output_v2.xlsx"
 
 #Run H2CS2
 run_H2CS2(input_filepath*input_filename, output_filepath*output_filename)
